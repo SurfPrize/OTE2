4a125e4e70a882f40823c62dd80f8778c647b754
Dragvid <55921056+Dragvid@users.noreply.github.com>
2021-11-27 14:07:10 +0000

    borboleta movement

--- Assets/Scripts/Butterfly/ButterflyBehaviour.cs
+++ Assets/Scripts/Butterfly/ButterflyBehaviour.cs
@@ -6,18 +6,72 @@ public class ButterflyBehaviour : MonoBehaviour
 {
     public float fallSpeed;
     public float glideSpeed;
-    //REACTION TIME
+    public float upSpeed;
+    public float reactionTime;
+    public float flapWingTime;
+    [HideInInspector] public Vector2 spawnDir;
+    private float timeForNextReaction;
+    private float currentFlapWingTime;
+    private int state;
+    private Vector2 direction;
     //DIRECTION
     // 
     void Start()
     {
-        
+        timeForNextReaction = reactionTime;
+        currentFlapWingTime = flapWingTime;
+        direction = Vector2.left;
+        state = 1;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        //gliding
-        transform.Translate((Vector2.down * Time.deltaTime * fallSpeed) + (Vector2.left * Time.deltaTime * glideSpeed));
+        switch (state)
+        {
+            case 1:
+                //gliding
+                transform.Translate((Vector2.down * Time.deltaTime * fallSpeed) + (direction * Time.deltaTime * glideSpeed));
+                ChangeState();
+                break;
+            case 2:
+                //flap wings
+                ChangeDir();
+                transform.Translate(Vector2.up * Time.deltaTime * upSpeed);
+                if (currentFlapWingTime > 0)
+                {
+                    currentFlapWingTime -= Time.deltaTime;
+                }
+                else
+                {
+                    currentFlapWingTime = flapWingTime;
+                    state = 1;
+                }
+                break;
+            default:
+                Debug.Log("Wrong number");
+                break;
+        }
+    }
+    private void ChangeState()
+    {
+        if (timeForNextReaction > 0)
+        {
+            timeForNextReaction -= Time.deltaTime;
+        }
+        else {
+            state = Random.Range(1, 3);
+            //Debug.Log("state: " + state);
+            timeForNextReaction = reactionTime;
+        }
+    }
+    private void ChangeDir()
+    {
+        if (Random.value < 0.5f)
+        {
+            direction = Vector2.right;
+        }
+        else
+        {
+            direction = Vector2.left;
+        }
     }
 }

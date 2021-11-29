fff3933e0341763032c90c4740fe597da4451b71
Ricardosousacardoso99 <55951004+Ricardosousacardoso99@users.noreply.github.com>
2021-11-29 16:22:47 +0000

    menu

--- Assets/mainMenu.cs
+++ Assets/mainMenu.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
+
+
+
+
 
 public class mainMenu : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public void  PlayGame()
     {
-        
-    }
+      
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }

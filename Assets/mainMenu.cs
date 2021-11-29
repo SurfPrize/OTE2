bb527bdd41bb154b4a157a5bc2c16f322d920e19
Ricardosousacardoso99 <55951004+Ricardosousacardoso99@users.noreply.github.com>
2021-11-29 17:48:19 +0000

    menu

--- Assets/mainMenu.cs
+++ Assets/mainMenu.cs
@@ -7,12 +7,22 @@ using UnityEngine;
 
 
 
+
 public class mainMenu : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     public void  PlayGame()
     {
       
 
     }
+
+    public void Exit()
+    {
+
+        Application.Quit();
+    }
+
+   
 }

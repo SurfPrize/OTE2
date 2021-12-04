abae05e97cce288f8d6d81930d2ed09fc68b6b2f
zezezezezezezezezezezezezeze <joselmorais@outlook.pt>
2021-12-04 18:39:47 +0000

    Update Score.cs

--- Assets/Scripts/POINTS/Score.cs
+++ Assets/Scripts/POINTS/Score.cs
@@ -7,11 +7,28 @@ public class Score : MonoBehaviour
 {
     public static int playerPoint = 0;
     public static TMP_Text scorePoint;
+    private static TMP_Text HPText;
+    private static int _hp = 10;
+
     [SerializeField] private TMP_Text scoreref;
+    [SerializeField] private TMP_Text hpref;
+
+    public static int Hp
+    {
+        get => _hp;
+
+        set
+        {
+            _hp = value;
+            HPText.text = "Player HP: " + Hp;
+        }
+    }
+
     // Start is called before the first frame update
     private void Awake()
     {
         scorePoint = scoreref;
+        HPText = hpref;
     }
     public static void AddPoint(int pontos)
     {

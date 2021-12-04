c82451f35a8e1b0283ff2b0f3930ca4d3bb58bb1
zezezezezezezezezezezezezeze <joselmorais@outlook.pt>
2021-12-04 20:44:28 +0000

    Butterflies em falta

--- Assets/Scripts/POINTS/ScoreSystem.cs
+++ Assets/Scripts/POINTS/ScoreSystem.cs
@@ -6,7 +6,8 @@ using TMPro;
 
 public class ScoreSystem : MonoBehaviour
 {
-
+    public int NormalScore = 1;
+    public int BlueMultiplier = 10;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         //if (collision.gameObject.name == "Orange Butterfly(Clone)")
@@ -22,13 +23,16 @@ public class ScoreSystem : MonoBehaviour
             switch (este.Tipo)
             {
                 case ButterflyType.ORANGE:
-                    Debug.Log("Detectei a borboleta");
-                    Score.AddPoint(1);
+                    // Debug.Log("Detectei a borboleta");
+                    Score.AddPoint(NormalScore);
                     DeleteButterfly(este.gameObject);
                     break;
                 case ButterflyType.RED:
+                    Score.Hp -= 1;
+                    DeleteButterfly(este.gameObject);
                     break;
                 case ButterflyType.BLUE:
+                    Score.AddPoint(NormalScore * BlueMultiplier);
                     break;
             }
         }
